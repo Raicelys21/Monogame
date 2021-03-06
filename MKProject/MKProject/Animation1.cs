688e150b2d30e4058df1777cddeba92ce2c0c22e
Raicelys Suero <72804397+Raicelys21@users.noreply.github.com>
2021-03-05 20:49:39 -0800

    1

---
+++ MKProject/MKProject/Animation1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MKProject
+{
+    class Animation1
+    {
+        Texture2D texture;
+        Texture2D stance;
+        Rectangle rectangle;
+        Vector2 position;
+        Vector2 origin;
+        Vector2 velocity;
+        int currentFrame;
+        int frameHeight;
+        int frameWidth;
+        int sup;
+        float timer;
+        float interval = 50;
+
+        public Animation1(Texture2D newTexture, Vector2 newPosition, int newFrameHeight, int newFrameWidth, Texture2D newStance, int newSup)
+        {
+            texture = newTexture;
+            position = newPosition;
+            frameHeight = newFrameHeight;
+            frameWidth = newFrameWidth;
+            stance = newStance;
+            sup = newSup;
+        }
+
+        public void Update(GameTime gameTime, Texture2D newTexture, int newFrameWidth)
+        {
+            rectangle = new Rectangle(currentFrame * frameWidth, 0, frameWidth, frameHeight);
+            origin = new Vector2(rectangle.Width / 2, rectangle.Height / 2);
+            position = position + velocity;
+            texture = newTexture;
+            frameWidth = newFrameWidth;
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                AnimateRight(gameTime);
+                velocity.X = 3;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                AnimateLeft(gameTime);
+                velocity.X = -3;
+
+            }
+            else
+            {
+                Animate(gameTime);
+                velocity = Vector2.Zero;
+            }
+
+            if (position.X < 32)
+            {
+                position.X = 32;
+            }
+            if (position.X > 768)
+            {
+                position.X = 768;
+            }
+        }
+        public void AnimateRight(GameTime gameTime)
+        {
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds / 2;
+            if (timer > interval)
+            {
+                currentFrame++;
+                timer = 0;
+                if (currentFrame > 8)
+                    currentFrame = 0;
+            }
+        }
+
+        public void AnimateLeft(GameTime gameTime)
+        {
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds / 2;
+            if (timer > interval)
+            {
+                currentFrame--;
+                timer = 0;
+                if (currentFrame > 8 || currentFrame == 0)
+                    currentFrame = 8;
+            }
+        }
+
+        public void Animate(GameTime gameTime)
+        {
+            texture = stance;
+            frameWidth = sup;
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds / 3;
+            if (timer >= interval)
+            {
+                if (currentFrame >= 5)
+                {
+                    currentFrame = 0;
+                }
+                else
+                {
+                    currentFrame++;
+                }
+                timer = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, rectangle, Color.White, 0f, origin, 1.0f, SpriteEffects.None, 0);
+        }
+    }
+}
---
+++ MKProject/MKProject/Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace MKProject
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Song song;
+        private Texture2D background;
+        Animation player;
+        Animation1 player1;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            player = new Animation(Content.Load<Texture2D>("forward1"), new Vector2(100, 400), 140, 73, Content.Load<Texture2D>("stance1"), 80);
+            player1 = new Animation1(Content.Load<Texture2D>("forward2"), new Vector2(600, 400), 140, 77, Content.Load<Texture2D>("stance2"), 83);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            background = Content.Load<Texture2D>("bakground");
+            song = Content.Load<Song>("music");
+            MediaPlayer.Play(song);
+            MediaPlayer.IsRepeating = true;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+            player.Update(gameTime, Content.Load<Texture2D>("forward1"), 73);
+            player1.Update(gameTime, Content.Load<Texture2D>("forward2"), 77);
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(background, new Rectangle(0, 0, 899, 489), Color.White);
+            player.Draw(_spriteBatch);
+            player1.Draw(_spriteBatch);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
